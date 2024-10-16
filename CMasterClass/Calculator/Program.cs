acb295b602033da82732add2f0bf03b9d02e841c
RicardoDL89 <90687037+RicardoDL89@users.noreply.github.com>
2024-10-16 15:13:02 +0200

    commited

---
+++ CMasterClass/Calculator/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Calculator");
+
+Console.WriteLine("Pick 1st Number");
+string userInput = Console.ReadLine();
+
+Console.WriteLine("You Entered " + userInput);
+
+Console.WriteLine("Pick 2nd Number");
+string userInput2 = Console.ReadLine();
+
+Console.WriteLine("You Entered " + userInput2);
+
+Console.WriteLine("Sum of Inputs is...");
+Console.WriteLine(userInput + userInput2);
+
+
+static void Check()
+{
+    Method();
+}
+
+
+
+///<summary>
+///This method does stuff
+/// </summary>
+static void Method ()
+{
+
+}
--- CMasterClass/Helloworld/Program.cs
+++ CMasterClass/Helloworld/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+//declare a variable
+string myFriendsName;
+
+myFriendsName = "Marcelo";
+
+Console.WriteLine(myFriendsName);
+
+myFriendsName = "John";
+
+Console.WriteLine(myFriendsName);
