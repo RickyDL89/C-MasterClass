b212b66a970c137065574fb1850dcb3cc1e82470
RicardoDL89 <90687037+RicardoDL89@users.noreply.github.com>
2024-10-21 09:25:16 +0200

    updated

--- CMasterClass/Calculator/obj/Debug/net8.0/Calculator.AssemblyInfo.cs
+++ CMasterClass/Calculator/obj/Debug/net8.0/Calculator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+acb295b602033da82732add2f0bf03b9d02e841c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05809a8f7fd6610590f5ea7b060408238a276ff7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- CMasterClass/Helloworld/obj/Debug/net8.0/Helloworld.AssemblyInfo.cs
+++ CMasterClass/Helloworld/obj/Debug/net8.0/Helloworld.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Helloworld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+acb295b602033da82732add2f0bf03b9d02e841c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05809a8f7fd6610590f5ea7b060408238a276ff7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Helloworld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Helloworld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
