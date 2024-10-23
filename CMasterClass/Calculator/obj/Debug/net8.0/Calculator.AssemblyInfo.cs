240fc4d70169398847d3fd17e9a3965a3ff285ff
RicardoDL89 <90687037+RicardoDL89@users.noreply.github.com>
2024-10-23 13:41:01 +0200

    updated foreach loop

--- CMasterClass/Calculator/obj/Debug/net8.0/Calculator.AssemblyInfo.cs
+++ CMasterClass/Calculator/obj/Debug/net8.0/Calculator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f55f08ef99e38866ceead8699a7c677e0287b72b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbc7f4d1b6dcf31e4539995236bdc7097d29447f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ CMasterClass/ForEachLoop/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string[] weekDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+Console.WriteLine("Length of array is : " + weekDays.Length);
+
+for (int i = 0; i < weekDays.Length; i++)
+{
+    Console.WriteLine(weekDays[i]);
+}
+
+foreach (string day in weekDays)
+{
+    Console.WriteLine(day);
+}
