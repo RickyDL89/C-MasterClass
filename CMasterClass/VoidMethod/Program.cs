a36ebc49fca82baf651682549e4f261ff7d85b4b
RicardoDL89 <90687037+RicardoDL89@users.noreply.github.com>
2024-10-24 16:35:09 +0200

    updated methods

--- CMasterClass/VoidMethod/Program.cs
+++ CMasterClass/VoidMethod/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Methods are structured like this
+// Modifiers returnType MethodName(Parameters){
+// Code Block
+// }
+
+//No Parameter
+void MyFirstMethod()
+{
+    Console.WriteLine("Method execution");
+}
+
+//Calling the method
+MyFirstMethod();
+
+Console.WriteLine("Outside Method");
+
+//1 Paramter
+void WriteSomething(string myString)
+{
+    Console.WriteLine(myString);
+}
+
+WriteSomething("This is my string");
